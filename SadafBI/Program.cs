045319ba635d1e9159ed183d8c2e8529a7f095e6
unknown <saraniazkhani.811012@gmail.com>
2024-02-12 13:04:37 +0330

    sql_changes

--- SadafBI/Program.cs
+++ SadafBI/Program.cs
@@ -10,10 +10,10 @@ namespace SadafBI
         static void Main(string[] args)
         {
             HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Add("X-CLIENT-TOKEN", "f445797e-a6b8-4f64-8e54-893c5722ebbd");
+            client.DefaultRequestHeaders.Add("X-CLIENT-TOKEN", "9cf56baa-d8a6-4334-b95b-250353e39c16");
 
             Console.WriteLine("Calling Web API...");
-            var responseTask = client.GetAsync("https://api.irbroker.com/api/v1/listCustomers?dsCode=765&modificationDateFrom=1390/01/01&creationDateFrom=1390/01/01&size=100&page=22");
+            var responseTask = client.GetAsync("https://api.irbroker.com/api/v1/listCustomers?dsCode=765&modificationDateFrom=1390/01/01&creationDateFrom=1390/01/01&size=100&page=741");
             responseTask.Wait();
 
             if (responseTask.IsCompleted)
@@ -33,21 +33,128 @@ namespace SadafBI
 
                 var apiResponse = JsonConvert.DeserializeObject<APIResponseModel>(responseContent, settings);
 
-                //var apiResponse = JsonConvert.DeserializeObject<APIResponseModel>(responseContent);
+                
                 using (var context = new DataContext())
                 {
                     foreach (var resultItem in apiResponse.result)
                     {
-                        // دریافت مدل مپ شده
-                        var mappedData = MappingHelper.MapResultToSqlModel(resultItem);
+                        var existingCustomer = context.Customers.FirstOrDefault(c => c.customerId == resultItem.customerId);
+                        if (existingCustomer != null)
+                        {
+                            // Update the existing record
+                            existingCustomer.nationalCode = resultItem.nationalCode ?? existingCustomer.nationalCode;
+                            existingCustomer.companyName = resultItem.companyName ?? existingCustomer.companyName;
+                            existingCustomer.firstName = resultItem.firstName ?? existingCustomer.firstName;
+                            existingCustomer.lastName = resultItem.lastName ?? existingCustomer.lastName;
+                            existingCustomer.fatherName = resultItem.fatherName ?? existingCustomer.fatherName;
+                            existingCustomer.phoneNumber = resultItem.phoneNumber ?? existingCustomer.phoneNumber;
+                            existingCustomer.mobileNumber = resultItem.mobileNumber ?? existingCustomer.mobileNumber;
+                            existingCustomer.emailAddress = resultItem.emailAddress ?? existingCustomer.emailAddress;
+                            existingCustomer.dlNumber = resultItem.dlNumber ?? existingCustomer.dlNumber;
+                            existingCustomer.status = resultItem.status ?? existingCustomer.status;
+                            existingCustomer.credit = resultItem.credit ?? existingCustomer.credit;
+                            existingCustomer.comexCredit =(int?) resultItem.comexCredit ?? existingCustomer.comexCredit;
+                            existingCustomer.sfCredit = (int?)resultItem.sfCredit ?? existingCustomer.sfCredit;
+                            existingCustomer.telegramUsername = (string)(resultItem.telegramUsername ?? existingCustomer.telegramUsername);
+                            existingCustomer.telegramStatusId = (string)(resultItem.telegramStatusId ?? existingCustomer.telegramStatusId);
+                            existingCustomer.creationDate = resultItem.creationDate ?? existingCustomer.creationDate;
+                            existingCustomer.branchId = resultItem.branchId;
+                            existingCustomer.branchName = resultItem.branchName ?? existingCustomer.branchName;
+                            existingCustomer.isCompany = resultItem.isCompany;
+                            existingCustomer.customerIdentity = resultItem.customerIdentity ?? existingCustomer.customerIdentity;
+                            existingCustomer.birthDate = resultItem.birthDate ?? existingCustomer.birthDate;
+                            existingCustomer.birthCertificationNumber = resultItem.birthCertificationNumber ?? existingCustomer.birthCertificationNumber;
+                            existingCustomer.address = resultItem.address ?? existingCustomer.address;
+                            existingCustomer.postalCode = resultItem.postalCode ?? existingCustomer.postalCode;
+                            existingCustomer.registerationNumber = resultItem.registerationNumber ?? existingCustomer.registerationNumber;
+                            existingCustomer.bourseAccountName = resultItem.bourseAccountName ?? existingCustomer.bourseAccountName;
+                            existingCustomer.accountNumber = resultItem.accountNumber ?? existingCustomer.accountNumber;
+                            existingCustomer.onlineUsername = resultItem.onlineUsername ?? existingCustomer.onlineUsername;
+                            existingCustomer.hasOnlineAccount = resultItem.hasOnlineAccount;
+                            existingCustomer.modificationDate = resultItem.modificationDate ?? existingCustomer.modificationDate;
+                            existingCustomer.isMmtpUser = resultItem.isMmtpUser;
+                            existingCustomer.mmtpUserStatusId = resultItem.mmtpUserStatusId ?? existingCustomer.mmtpUserStatusId;
+                            existingCustomer.isSiteUser = resultItem.isSiteUser;
+                            existingCustomer.eorderStatusId = resultItem.eorderStatusId;
+                            existingCustomer.sexTypeId = resultItem.sexTypeId;
+                            existingCustomer.sexTypeName = resultItem.sexTypeName ?? existingCustomer.sexTypeName;
+                            existingCustomer.referredBy = resultItem.referredBy ?? existingCustomer.referredBy;
+                            existingCustomer.hasSignSample = resultItem.hasSignSample;
+                            existingCustomer.hasCustomerPhoto = resultItem.hasCustomerPhoto;
+                            existingCustomer.hasBirthCertificate = resultItem.hasBirthCertificate;
+                            existingCustomer.hasCertificateComments = resultItem.hasCertificateComments;
+                            existingCustomer.hasZipFile = resultItem.hasZipFile;
+                            existingCustomer.hasOfficialGazette = resultItem.hasOfficialGazette;
+                            existingCustomer.hasOfficialAds = resultItem.hasOfficialAds;
+                            existingCustomer.comexVisitorId = (string)(resultItem.comexVisitorId ?? existingCustomer.comexVisitorId);
+                            existingCustomer.mmtpUserId = resultItem.mmtpUserId ?? existingCustomer.mmtpUserId;
+                            existingCustomer.comexEconomyAccount = resultItem.comexEconomyAccount ?? existingCustomer.comexEconomyAccount;
+                            existingCustomer.isPortfo = resultItem.isPortfo;
+                            existingCustomer.traderCredit = (int?)(resultItem.traderCredit ?? existingCustomer.traderCredit);
+                            existingCustomer.provinceCode = resultItem.provinceCode ?? existingCustomer.provinceCode;
+                            existingCustomer.provinceName = resultItem.provinceName ?? existingCustomer.provinceName;
+                            existingCustomer.cityId = resultItem.cityId;
+                            existingCustomer.cityCode = resultItem.cityCode ?? existingCustomer.cityCode;
+                            existingCustomer.cityName = resultItem.cityName ?? existingCustomer.cityName;
+                            existingCustomer.bankAccountId = resultItem.bankAccountId ?? existingCustomer.bankAccountId;
+                            existingCustomer.bankAccountNumber = resultItem.bankAccountNumber ?? existingCustomer.bankAccountNumber;
+                            existingCustomer.shabaNumber = resultItem.shabaNumber ?? existingCustomer.shabaNumber;
+                            existingCustomer.baTypeName = resultItem.baTypeName ?? existingCustomer.baTypeName;
+                            existingCustomer.bankName = resultItem.bankName ?? existingCustomer.bankName;
+                            existingCustomer.isStockCreditPurchase = resultItem.isStockCreditPurchase;
+                            existingCustomer.isCollateralStocksCustomer = resultItem.isCollateralStocksCustomer;
+                            existingCustomer.isProfessionalTrader = resultItem.isProfessionalTrader;
+                            existingCustomer.isOtcProfessionalTrader = resultItem.isOtcProfessionalTrader;
+                            existingCustomer.comments = resultItem.comments ?? existingCustomer.comments;
 
-                        // اضافه کردن به دیتابیس
-                        context.Customers.Add(mappedData);
+                            // Update the associated domain and customer group if necessary
+                            if (resultItem.domains != null && resultItem.domains.Length > 0)
+                            {
+                                existingCustomer.SqlDomain = new SqlDomain
+                                {
+                                    domainId = resultItem.domains[0].domainId,
+                                    domainName = resultItem.domains[0].domainName
+                                };
+                            }
+
+                            if (resultItem.customerGroups != null && resultItem.customerGroups.Length > 0)
+                            {
+                                existingCustomer.SqlCustomergroup = new SqlCustomergroup
+                                {
+                                    customerGroupId = resultItem.customerGroups[0].customerGroupId,
+                                    customerGroupName = resultItem.customerGroups[0].customerGroupName
+                                };
+                            }
+                        }
+                        else
+                        {
+                            // Create a new record
+                            var mappedData = MappingHelper.MapResultToSqlModel(resultItem);
+                            context.Customers.Add(mappedData);
+                        }
                     }
 
-                    // ذخیره تغییرات در دیتابیس
                     context.SaveChanges();
+
+                    //foreach (var resultItem in apiResponse.result)
+                    //{
+                    //    // دریافت مدل مپ شده
+                    //    var mappedData = MappingHelper.MapResultToSqlModel(resultItem);
+
+                    //    // اضافه کردن به دیتابیس
+                    //    context.Customers.Add(mappedData);
+                    //}
+
+                    //// ذخیره تغییرات در دیتابیس
+                    //context.SaveChanges();
                 }
+
+            }
+        }
+    }
+
+}
+
 
                 //using (var context = new DataContext())
                 //{
@@ -63,13 +170,6 @@ namespace SadafBI
                 //    // ذخیره تغییرات در دیتابیس
                 //    context.SaveChanges();
                 //}
-
-            }
-        }
-    }
-
-}
-
 
 
 
